0e7ff0d9426e072a140cbc0122001607abd261da
rocket-man <jluca00@gmail.com>
2017-01-07 23:46:43 +0000

    Remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from Adafruit_LED_Backpack import SevenSegment
-
-display = SevenSegment.SevenSegment()
-
-for x in range(0, 9999):
-	display.clear()
-	display.write_display()
-	display.print_number_str(str(x))
-	display.write_display()
